e55e65ac07c7986d35edc119ed2f271cbb887b69
JvHaeckel <joaohaeckel@gmail.com>
2023-05-11 07:57:20 -0300

    Subindo

--- JavaScript/Interagindo User/interagindoUserArray.js
+++ JavaScript/Interagindo User/interagindoUserArray.js
@@ -1,7 +1,38 @@
 
+// var segredos = [5,7,10,2,3];
+
+// var input = document.querySelector("input");
+//     // input.focus();
+
+// function verifica() {
+
+// var achou = false;
+
+// for(var posicao = 0; posicao < segredos.length; posicao++) {
+
+//     if(input.value == segredos[posicao]) {
+
+//         alert("Você ACERTOU!");
+//         achou = true;
+//         break;
+//     }
+// }
+
+// if(achou == false) {
+//     alert("Você ERROU!");
+// }
+
+// input.value = "";
+// input.focus();
+
+// }
+
+// var button = document.querySelector("button");
+
+// button.onclick = verifica();
 
 let input = document.getElementById("input");
-input.focus(); /* Assim que entrar na página o teclado permanece no input */
+// input.focus(); /* Assim que entrar na página o teclado permanece no input */
 
 let secrets = [2, 4, 6, 8];
 
